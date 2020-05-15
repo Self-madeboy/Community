1c52d2fa729ce1e0b0440ecf5adaab64049b455b
Self-madeboy <1738203391@qq.com>
2020-05-15 10:47:43 +0800

    first commit 1

---
+++ src/test/java/love/jc/community/first/FirstApplicationTests.java
@@ -0,0 +1,13 @@
+package love.jc.community.first;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
